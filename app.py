625dae6f7ec3e4fefe0c55685fe5c498466727f5
Sam-joshi-11 <samarthjoshi89517@gmail.com>
2026-07-07 14:40:30 +0530

    ..

--- app.py
+++ app.py
@@ -37,15 +37,6 @@ def health():
 # prediction endpoint
 @app.post("/predict")
 def predict(data: LoanRequest):
-    """input_data = [[data.Age,data.Salary]]
-    prediction = model.predict(input_data)
-    
-    if prediction[0] == 1:
-        result = 'Loan Approved'
-    else:
-        result = 'Loan Rejected'
-    
-    return {"prediction": result}"""
     # check model availability
     if model is None:
         raise HTTPException(
